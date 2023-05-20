0c06bc1c509a6060f1787339ce0bfe518f78b606
xtLUCIFER <umr0042@gmail.com>
2023-05-20 11:17:34 +0500

    database model & logic created

--- app/models.py
+++ app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+import uuid
 # Create your models here.
+
+
+class CustomUser(AbstractUser): 
+    name = None
+    email = models.EmailField(unique=True)
+    profile = models.ManyToManyField('Profile',blank=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username',]
+
+    def __str__(self):
+        return self.email
+
+    
+AGE_CHOICES = (
+    ('All','All'),
+    ('Kids','Kids')
+)
+MOVIE_CHOICES = (
+    ('seasonal','Seasonal'),
+    ('Single','Single')
+)
+class Profile(models.Model):
+    name = models.CharField(max_length=255)
+    age_limit = models.CharField(max_length=10,choices=AGE_CHOICES)
+    uuid = models.UUIDField(default=uuid.uuid4)
+
+class Movie(models.Model):
+    name = models.CharField(max_length=255)
+    discription = models.TextField(null=True,blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    uuid = models.UUIDField(default=uuid.uuid4)
+    type = models.CharField(max_length=10, choices=MOVIE_CHOICES)
+    video = models.ManyToManyField('Video')
+    flyer= models.ImageField(upload_to='flyers')
+    age_limit = models.CharField(max_length=10,choices=AGE_CHOICES)
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=255,null=True,blank=True)
+    file = models.FileField(upload_to='movies')
--- netflix/settings.py
+++ netflix/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'netflix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,9 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static_root'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'app.CustomUser'
